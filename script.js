dec77c8457e309144bf16769c43c0f799ffe12d8
Ashwinimorale13 <140158120+Ashwinimorale13@users.noreply.github.com>
2023-08-29 13:59:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Darkmode
+document.querySelector('.toggle-btn').addEventListener('click', () => {
+    document.body.classList.toggle('darkmode');
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "submitted";
+  }
